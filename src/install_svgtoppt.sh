dda53981bddf7f961d8ab2947d1ed819f9e2286d
Blake Gearin <blakegearin@pm.me>
2021-03-03 00:33:18 -0600

    Preparing alpha37

--- src/install_svgtoppt.sh
+++ src/install_svgtoppt.sh
@@ -1,5 +1,5 @@
 # APPLICATION CONFIG VALUES
-version=1.0.0-alpha36
+version=1.0.0-alpha37
 application_name=svgtoppt
 application_directory=$PWD/$application_name
 application_config_file=$application_name
